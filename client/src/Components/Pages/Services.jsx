43b7442dbaa606656d61b42e6ecf6b2361063f25
Morel-D <morelDen@gmail.com>
2022-12-13 02:40:12 -0800

    Service done move to Skill layer

--- client/src/Components/Pages/Services.jsx
+++ client/src/Components/Pages/Services.jsx
@@ -1,7 +1,54 @@
 const Services = () => {
     return ( 
-        <div>
-            This are my services
+        <div className="text-white">
+            
+            <div className="title px-5 py-4 mx-5">
+            <h2 className="titleServices ">My Services</h2>
+            <label className="lead">Get creative and enjoy the most dedicated development and designs services</label>
+            </div>
+
+            <div className="row p-5 text-center">
+                <div className="col">
+                    <div className="card cardService">
+                        <h4 className="cardServiceTitle mt-3">Web Develepment</h4>
+                        <p className="p-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et asperiores facere aperiam animi,
+                            molestiae maiores sequi minima labore tempora
+                        </p>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card cardService">
+                        <h4 className="cardServiceTitle mt-3">Android Develepment</h4>
+                        <p className="p-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et asperiores facere aperiam animi,
+                            molestiae maiores sequi minima labore tempora
+                        </p>
+                    </div>
+                </div>
+            
+
+            <div className="col">
+                    <div className="card cardService">
+                        <h4 className="cardServiceTitle mt-3">UI Design</h4>
+                        <p className="p-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et asperiores facere aperiam animi,
+                            molestiae maiores sequi minima labore tempora
+                        </p>
+                    </div>
+            </div>
+
+            <div className="col">
+                    <div className="card cardService">
+                        <h4 className="cardServiceTitle mt-3">Graphic Design</h4>
+                        <p className="p-1">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et asperiores facere aperiam animi,
+                            molestiae maiores sequi minima labore tempora
+                        </p>
+                    </div>
+            </div>
+        </div>    
         </div>
      );
 }
